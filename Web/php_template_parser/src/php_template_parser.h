23d8cb96b40e670a1123b5a3ab6d26e8c88cf58c
FxYCarl <fxycarl@gmail.com>
2013-03-03 22:10:45 +0800

    Parser Entry Finished

--- Web/php_template_parser/src/php_template_parser.h
+++ Web/php_template_parser/src/php_template_parser.h
@@ -1,3 +1,20 @@
+#ifndef _PHP_TEMPLATE_PARSER_H
+#define _PHP_TEMPLATE_PARSER_H
 
+#include "php.h"
+
+#define PHP_TEMPLATE_PARSER_VERSION 1.0
+
+extern zend_module_entry template_parser_module_entry;
+
+//PHP_MINIT_FUNCTION(template_parser);
+//PHP_MSHUTDOWN_FUNCTION(template_parser);
+//PHP_MINFO_FUNCTION(template_parser);
+//PHP_RINIT_FUNCTION(template_parser_request);
+//PHP_RSHUTDOWN_FUNCTION(template_parser_request);
+
+PHP_FUNCTION(template_parser_pause);
+
+#endif
 
 
--- Web/php_template_parser/src/template_parser.c
+++ Web/php_template_parser/src/template_parser.c
@@ -1,3 +1,33 @@
+#include "php_template_parser.h"
 
+
+ZEND_BEGIN_ARG_INFO_EX(template_parser_pause_args,ZEND_ZEND_BY_VAL,ZEND_RETURN_VAL,2)
+    ZEND_ARG_INFO(0,object)
+    ZEND_ARG_INFO(0,template)
+    ZEND_ARG_INFO(0,param)
+    ZEND_ARG_INFO(0,openTest)
+ZEND_END_ARG_INFO()
+
+zend_function_entry template_parser_functions[] = {
+    PHP_FE(template_parser_pause,template_parser_pause_args)
+    PHP_FE_END
+}
+
+zend_module_entry template_parser_module_entry = {
+    STANDARD_MODULE_HEADER,
+    "template_parser",
+    template_parser_functions,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    PHP_TEMPLATE_PARSER_VERSION,
+    STANDARD_MODULE_PROPERTIES
+}
+
+PHP_FUNCTION(template_parser_pause){
+    
+}
 
 
