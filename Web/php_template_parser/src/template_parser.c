a922ef6400708d266c72926f79737894e36915d2
FxYCarl <fxycarl@gmail.com>
2013-03-12 12:11:01 +0800

    Extension Planed

--- Web/php_template_parser/src/template_parser.c
+++ Web/php_template_parser/src/template_parser.c
@@ -1,10 +1,15 @@
-#include "php_template_parser.h"
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
+#include "php.h"
+
+#include "php_template_parser.h"
 
 ZEND_BEGIN_ARG_INFO_EX(template_parser_pause_args,ZEND_ZEND_BY_VAL,ZEND_RETURN_VAL,2)
     ZEND_ARG_INFO(0,object)
     ZEND_ARG_INFO(0,template)
-    ZEND_ARG_INFO(0,param)
+    ZEND_ARG_ARRAY_INFO(0,param,1)
     ZEND_ARG_INFO(0,openTest)
 ZEND_END_ARG_INFO()
 
@@ -27,6 +32,50 @@ zend_module_entry template_parser_module_entry = {
 }
 
 PHP_FUNCTION(template_parser_pause){
+    zval *object = NULL;
+    char *template = NULL;
+    zval *param = NULL;
+    int template_length = 0;
+    zend_bool openTest = 0;
+    int process_start = 0;
+
+    if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"os|ab",&object,&template,&template_length,&param) == FAILURE){
+        return ;
+    }
+   
+    for (process_start = 0;process_start <= template_length;){
+        switch(){
+            
+        }
+    }
+
+}
+
+static int template_parser_extract_param(){
+    
+}
+static int template_parser_add_return_string(){
+    
+}
+static int template_parser_find_token(){
+    
+}
+static int template_parser_get_process_start(){
+    
+}
+static inline int template_parser_process_token_if(){
+    
+}
+static inline int template_parser_process_token_for(){
+    
+}
+static inline int template_parser_process_token_switch(){
+    
+}
+static inline int template_parser_process_token_while(){
+    
+}
+static inline int template_parser_process_token_foreach(){
     
 }
 
