51c0b88d6e7498421890ddc29fbbd88a2a788195
FxYCarl <fxycarl@gmail.com>
2013-03-25 22:19:17 +0800

    php_template_parser fix

--- Web/php_template_parser/src/php_template_parser.h
+++ Web/php_template_parser/src/php_template_parser.h
@@ -3,8 +3,6 @@
 
 #include "php.h"
 
-#define PHP_TEMPLATE_PARSER_VERSION 1.0
-
 #ifdef ZTS
 #include "TSRM.h"
 #endif
@@ -16,6 +14,7 @@
 #endif
 
 extern zend_module_entry template_parser_module_entry;
+extern ZEND_DECLARE_MODULE_GLOBALS(template_parser);
 
 //PHP_MINIT_FUNCTION(template_parser);
 //PHP_MSHUTDOWN_FUNCTION(template_parser);
@@ -23,22 +22,57 @@ extern zend_module_entry template_parser_module_entry;
 //PHP_RINIT_FUNCTION(template_parser_request);
 //PHP_RSHUTDOWN_FUNCTION(template_parser_request);
 
-struct _TEMPLATE_PARSER_OUTPUT_{
+struct _TEMPLATE_PARSER_PARSE_RESULT_BUFFER_{
     char *string;
     unsigned long length;
 };
 
-typedef _TEMPLATE_PARSER_OUTPUT_ template_parser_output;
+typedef _TEMPLATE_PARSER_PARSE_RESULT_BUFFER_ template_parser_parse_result_buffer;
 
 ZEND_BEGIN_MODULE_GLOBALS(template_parser)
-    template_parser_output *result_string;
+    template_parser_parse_result_buffer *result_buffer;
 ZEND_END_MODULE_GLOBALS(template_parser)
 
+PHP_FUNCTION(template_parser_parse);
 
+#define TEMPLATE_PARSER_PARSE_STORE_RESULT_BUFFER_AND_OUTPUT_HANDLER(FUNCTION) \
+    template_parser_parse_result_buffer *stored_result_buffer = TEMPLATE_PARSER_G(result_buffer); \
+    int (*stored_output_func)(const char *str,uint length TSRMLS_DC) = OG(php_body_write) \
+    TEMPLATE_PARSER_G(result_buffer) = NULL; \
+    OG(php_body_write) = FUNCTION;
 
-extern ZEND_DECLARE_MODULE_GLOBALS(template_parser);
+#define TEMPLATE_PARSER_PARSE_RESTORE_RESULT_BUFFER_AND_OUTPUT_HANDLER() \
+    TEMPLATE_PARSER_G(result_buffer) = result_buffer; \
+    OG(php_body_write) = stored_output_func;
 
-PHP_FUNCTION(template_parser_pause);
+#define TEMPLATE_PARSER_PARSE_STORE_ENV() \
+    HashTable *stored_active_symbol_table; \
+    zend_class_entry *stored_scope; \
+    if(EG(active_symbol_table)) { \
+        stored_active_symbol_table = EG(active_symbol_table); \
+    } else { \
+        stored_active_symbol_table = NULL; \
+    } \
+    stored_scope = EG(scope); \
+    ALLOC_HASHTABLE(EG(active_symbol_table)); \
+    zend_hash_init(EG(active_symbol_table),0,NULL,ZVAL_PTR_DTOR,0); \
+
+#define TEMPLATE_PARSER_PARSE_RESTORE_ENV() \
+    HashTable *free_active_symbol_table = EG(active_symbol_table); \
+    EG(scope) = stored_scope; \
+    EG(active_symbol_table) = stored_active_symbol_table; \
+    zend_hash_destory(free_active_symbol_table); \
+    FREE_HASHTABLE(free_active_symbol_table);
+
+#define TEMPLATE_PARSER_PARSE_STORE_OPCODE_ENV() \
+    zend_op_array *stored_op_array = EG(active_op_array); \
+    zend_op **stored_opline_ptr = EG(opline_ptr); \
+    zval **stored_return_value_ptr_ptr = EG(return_value_ptr_ptr);
+
+#define TEMPLATE_PARSER_PARSE_RESTORE_OPCODE_ENV() \
+    EG(active_op_array) = stored_op_array; \
+    EG(opline_ptr) = stored_opline_ptr; \
+    EG(return_value_ptr_ptr) = stored_return_value_ptr_ptr;
 
 #endif
 
--- Web/php_template_parser/src/template_parser.c
+++ Web/php_template_parser/src/template_parser.c
@@ -1,7 +1,6 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
-
 #include "php.h"
 #include "main/php_output.h"
 
@@ -12,6 +11,7 @@ ZEND_DECLARE_MODULE_GLOBALS(template_parser)
 #ifdef COMPILE_SO_TEMPLATE_PARSER
 ZEND_GET_MODULE(template_parser)
 #endif
+
 ZEND_BEGIN_ARG_INFO_EX(template_parser_pause_args,ZEND_ZEND_BY_VAL,ZEND_RETURN_VAL,2)
     ZEND_ARG_INFO(0,object)
     ZEND_ARG_INFO(0,template)
@@ -39,91 +39,70 @@ zend_module_entry template_parser_module_entry = {
 
 PHP_FUNCTION(template_parser_pause){
     //Input params.
-    zval *object = NULL;
+    zval *object_container = NULL;
     char *template = NULL;
     zval *param = NULL;
     int template_length = 0;
     zend_bool openTest = 0;
-    //Storage of default callback
-    int(*template_parser_output_func)(const *str,uint length TSRMLS_DC);
-    //Storage 4 Opcode.
-    zend_op_array *execute_array;
-    zend_op_array *callback_op_array;
-    zend_op **callback_opline_ptr;
-    zval **callback_return_value_ptr_ptr;
-    HashTable *callback_symbol_table;
+    zend_object *real_object = NULL;
+    //Result.
+    template_parser_parse_result_buffer *result = NULL;
+    //Opcode.
+    zend_op_array *execute_array = NULL;
     zval template_zval;
-    char *desc;
-
-    //Free mem which have been allocated to store output.
-    template_parser_output *free = TEMPLATE_PARSER_G(result_string);
-    efree(free->string);
-    efree(free);
-    free = NULL;
-    TEMPLATE_PARSER_G(result_string) = NULL;
+    char *desc = NULL;
+    
+    TEMPLATE_PARSER_PARSE_STORE_RESULT_BUFFER_AND_OUTPUT_HANDLER(template_parser_output_writer);
 
     //Fetch parameters.
-    if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"os|ab",&object,&template,&template_length,&param,&openTest) == FAILURE){
+    if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"os|ab",&object_container,&template,&template_length,&param,&openTest) == FAILURE){
         return ;
     }
-
-    //redirect php output.
-    template_parser_output_func = OG(php_body_write);
-    OG(php_body_write) = template_parser_output_writer;
-    //Store default symbol table & scope.
-    //EG(scope)暂时继承原来的上下文.
-    if(EG(active_symbol_table)){
-        callback_symbol_table = EG(active_symbol_table);
+    //Fetch real object in order to fetch it's scope.
+    if(Z_TYPE_P(object_container) == IS_OBJECT){
+        real_object = Z_OBJVAL_P(object_container);
     } else {
-        callback_symbol_table = NULL; 
+        real_object = NULL;
     }
-    ALLOC_HASHTABLE(EG(active_symbol_table));
-    zend_hash_init(EG(active_symbol_table),0,NULL,ZVAL_PTR_DTOR,0);
-    //extract params.
-    template_parser_extract_param(param TSRMLS_CC);
-    //Compile & execute template.
+
     if(template_length){
+        TEMPLATE_PARSER_PARSE_STORE_ENV();
+        //Format template zval.
         INIT_ZVAL(template_zval);
         Z_TYPE(template_zval) = IS_STRING;
-        Z_STRLEN(template_zval) = template_length;
         Z_STRVAL(template_zval) = emalloc(template_length+1);
-        snprintf(Z_STRVAL(template_zval),Z_STRLEN(template_zval)+1,"%s",template);
-
-        desc = zend_make_compiled_string_description("template string" TSRMLS_CC);
+        Z_STRLEN(template_zval) = template_length;
+        snprintf(Z_STRVAL(template_zval),template_length+1,"%s",template);
+        //Format desc.
+        desc = zend_make_compiled_string_description("Template" TSRMLS_CC);
+        //Compile template.
         execute_array = zend_compile_string(&template_zval,desc TSRMLS_CC);
-        efree(desc);
-        zval_dtor(&template_zval);
 
         if(execute_array){
-            //Before execute save opcode params.
-            callback_op_array = EG(active_op_array);
-            callback_opline_ptr = EG(opline_ptr);
-            callback_return_value_ptr_ptr = EG(return_value_ptr_ptr);
-
+            TEMPLATE_PARSER_PARSE_STORE_OPCODE_ENV();
             EG(active_op_array) = execute_array;
             zend_execute(execute_array TSRMLS_CC);
 
-            //After execute restore opcode params.
-            EG(return_value_ptr_ptr) = callback_return_value_ptr_ptr;
-            EG(opline_ptr) = callback_opline_ptr;
-            EG(active_op_array) = callback_op_array;
+            TEMPLATE_PARSER_PARSE_RESTORE_OPCODE_ENV();
         }
-        destroy_op_array(execute_array TSRMLS_CC);
+        destory_op_array(execute_array TSRMLS_CC);
         efree(execute_array);
-    }
-    //Recover default symbol table & scope.
-    if(EG(active_symbol_table)){
-        zend_hash_destory(EG(active_symbol_table));
-        FREE_HASHTABLE(EG(active_symbol_table));
-    }
-    EG(active_symbol_table) = callback_symbol_table;
-    //EG(scope)暂时继承原来的上下文.
-    //give php output back to normal.
-    OG(php_body_write) = template_parser_output_func;
-    
-    //Return processed template string and give it back to PHP.
-    RETURN_STRINGL(TEMPLATE_PARSER_G(string),TEMPLATE_PARSER_G(length),0);
+        efree(desc);
+        zval_dtor(&template_zval);
 
+        TEMPLATE_PARSER_PARSE_RESTORE_ENV();
+    }
+
+
+    //Return processed template string and give it back to PHP.
+    result = TEMPLATE_PARSER_G(result_buffer);
+    TEMPLATE_PARSER_PARSE_RESTORE_RESULT_BUFFER_AND_OUTPUT_HANDLER();
+    //TODO: result leak memory every time it's called. Must be solved.
+    if(template_length){
+        RETURN_STRINGL(result->string,result->length,0);
+    } else {
+        return 1;
+    }
 }
 static int template_parser_extract_param(zval *param TSRMLS_DC){
     HashPosition it_pos;
@@ -152,40 +131,32 @@ static int template_parser_extract_param(zval *param TSRMLS_DC){
     return 1;
 }
 static int template_parser_output_writer(const char *str,uint length TSRMLS_DC){
-    unsigned long size = 0;
-    template_parser_output *source = TEMPLATE_PARSER_G(result_string);
-    template_parser_output *dest = NULL;
-    char *realloc_mem = NULL;
-    char *copy_start = NULL;
+    template_parser_parse_result_buffer *source;
+    template_parser_parse_result_buffer *dest;
+    unsigned long total_length;
 
-    //Input exists.
     if(str){
-        if(!source){
-            dest = (template_parser_output *)emalloc(sizeof(template_parser_output));
-            dest->string = (char *)emalloc(sizeof(length+1));
-            if(!dest->string){
-                //TODO: Error reporting.
-                return 1;
+        source = TEMPLATE_PARSER_G(result_buffer);
+        if(source){
+            source->string = (char *)emalloc(source->string,source->length+length+1);
+            if(source->string){
+                dest = source->string+source->length;
             }
-            dest->length = length+1;
-            copy_start = dest->string;
         } else {
-             realloc_mem = (char *)realloc(source->string,source->length+length);
-             if(!realloc_mem){
-                //TODO: Error reporting.
-                return 1;
-             }
-             dest->length = source->length+length;
-             dest->string = realloc_mem;
-             copy_start = dest->string+source->length-1;
+            source = (template_parser_parse_result_buffer *)emalloc(sizeof(template_parser_parse_result_buffer));
+            source->string = (char *)emalloc(length+1);
+            if(source&&source->string){
+                dest = source->string;
+            }
         }
-        memcpy(copy_start,str,length);
-        dest->string[dest->length+length]='\0';
-        TEMPLATE_PARSER_G(result_string) = dest;
-        return length;
-        
+        memcpy(dest,str,length);
+        source->string[total_length] = '\n';
+        TEMPLATE_PARSER_G(result_buffer) = source;
+
+        return 0;
     } else {
+        //TODO:Error handling.
         return 1;
-    }
+    } 
 }
 
